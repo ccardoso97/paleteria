137ce5bd6b98d3a9de4b6e5589ce925ca2a75266
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-04-14 20:34:12 -0300

    modificações no input e botão de pesquisa

--- script.js
+++ script.js
@@ -1,60 +1,57 @@
-const baseURL = "http://localhost:3000/paletas";
+const baseUrl = 'http://localhost:3000/paletas';
 
 async function findAllPaletas() {
-  const response = await fetch(`${baseURL}/find-paletas`);
+  const response = await fetch(`${baseUrl}/find-paletas`);
   const paletas = await response.json();
+
   paletas.forEach((paleta) => {
-    document.getElementById("paletaList").insertAdjacentHTML(
-      "beforeend",
+    document.getElementById('paletaList').insertAdjacentHTML(
+      'beforeend',
       `
         <div class="PaletaListaItem">
-            <div>
-                <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
-                <div class="PaletaListaItem__preco">R$${paleta.preco.toFixed(
-                  2
-                )}</div>
-                <div class="PaletaListaItem__descricao">${
-                  paleta.descricao
-                }</div>
+          <div>
+            <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
+            <div class="PaletaListaItem__preco">R$ ${paleta.preco.toFixed(
+              2,
+            )}</div>
+            <div class="PaletaListaItem__descricao">
+              ${paleta.descricao}
             </div>
-        </div>   
-        <img 
+          </div>
+          <img
+            class="PaletaListaItem__foto"
             src=${paleta.foto}
-            alt=${`Paleta de ${paleta.foto}`}
-            class="PaletaListaItem_foto"
-        />
-        `
+            alt=${`Paleta de ${paleta.sabor}`}
+          />
+        </div>
+      `,
     );
   });
 }
 
 findAllPaletas();
 
-// Pegar a paleta pelo ID
 const findByIdPaletas = async () => {
-  const id = document.getElementById("idPaleta").value;
-  const response = await fetch(`${baseURL}/find-paletas/${id}`);
-  const paleta = response.json();
-  const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
-  paletaEscolhidaDiv.innerHTML = 
-  `
-  <div class="PaletaCardItem">
-  <div>
-      <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
-      <div class="PaletaCardItem__preco">R$${paleta.preco.toFixed(
-        2
-      )}</div>
-      <div class="PaletaCardItem__descricao">${
-        paleta.descricao
-      }</div>
-  </div>
-</div>   
-<img 
-  src=${paleta.foto}
-  alt=${`Paleta de ${paleta.foto}`}
-  class="PaletaCardItem_foto"
-/>
+  const id = document.getElementById('idPaleta').value;
 
-  
-  `
+  const response = await fetch(`${baseUrl}/find-paletas/${id}`);
+
+  const paleta = await response.json();
+
+  const paletaEscolhidaDiv = document.getElementById('paletaEscolhida');
+
+  paletaEscolhidaDiv.innerHTML = `
+    <div class="PaletaCardItem">
+      <div>
+        <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+        <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
+        <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+      </div>
+      <img
+        class="PaletaCardItem__foto"
+        src=${paleta.foto}
+        alt=${`Paleta de ${paleta.sabor}`}
+      />
+    </div>
+  `;
 };
