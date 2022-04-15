ce84bdcfe58ae7f43e8bfff409fb91bf07d7003d
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-04-14 21:07:17 -0300

    configurando as rotas

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const route = require('./src/routes/paletas.route');
 
 const port = 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/paletas', route);
 
 const paletas = [
   {
     id: 1,
-    sabor: "Morango com Leite",
-    descricao: "Deliciosa paleta de morango com leite",
-    foto: "./assets/images/morango-com-leite-condensado.png",
+    sabor: 'Morango com Leite',
+    descricao: 'Deliciosa paleta de morango com leite',
+    foto: './assets/images/morango-com-leite-condensado.png',
     preco: 10.0,
   },
   {
     id: 2,
-    sabor: "Chocolate Branco",
-    descricao: "Deliciosa paleta de Chocolate Branco",
-    foto: "./assets/images/chocolate-branco.png",
+    sabor: 'Chocolate Branco',
+    descricao: 'Deliciosa paleta de Chocolate Branco',
+    foto: './assets/images/chocolate-branco.png',
     preco: 8.0,
   },
   {
     id: 3,
-    sabor: "Açaí com leite condensado",
-    descricao: "Açaí com leite condensado",
-    foto: "./assets/images/acai-com-leite-condensado.png",
+    sabor: 'Açaí com leite condensado',
+    descricao: 'Açaí com leite condensado',
+    foto: './assets/images/acai-com-leite-condensado.png',
     preco: 11.0,
   },
 ];
 
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.render(paletas);
 });
-app.get("/paletas/find-paletas", (req, res) => {
+app.get('/paletas/find-paletas', (req, res) => {
   res.send(paletas);
 });
-app.get("/paletas/find-paletas/:id", (req, res) => {
+app.get('/paletas/find-paletas/:id', (req, res) => {
   const idParam = req.params.id;
   const chosenPaleta = paletas.find((paleta) => paleta.id == idParam);
   res.send(chosenPaleta);
